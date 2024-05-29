b81b37ce6ee26fa4b28693a967422b69d4cf19ea
Her Fri <herbert.frichert@outlook.de>
2024-05-29 20:41:43 +0200

    add Project and skills components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { NavBar } from "./components/NavBar";
+import { Skills } from "./components/Skills";
+//import { Projects } from "./components/Projects";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Skills />
     </div>
   );
 }
